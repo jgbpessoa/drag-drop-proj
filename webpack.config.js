f40945a4a95024445e94878757902c13ff481a08
jgbpessoa <jgbpessoa@gmail.com>
2022-07-03 17:25:38 -0300

    Webpack Configuration: ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 // First remove .js from imports since Webpack will take care of file extension
 
 // Set Entry and Output Configuration
-// Use Node.js module (path) to build an absolute path
+// Use Node.js module to build an absolute path
 const path = require("path");
 
 module.exports = {
@@ -9,5 +9,26 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  // Make sure you have the sourceMap: true in tsconfig
+  // This tells Webpack that there will be generated sourcemaps already, which it should extract and basically wire up correctly to the bundle it generates
+  devtool: "inline-source-map",
+  // Tell webpack how to work with the files it finds
+  module: {
+    // Rules to be applied to the files
+    rules: [
+      {
+        // Test webpack will perform on any file to find out whether this rule here applies or not. It is a regular expression -> In this case we are telling that any file that ends with .ts should apply here
+        test: /\.ts$/,
+        // Tell what webpack should do with those files
+        use: "ts-loader",
+        // Ignore .ts files in node modules. It is a regular expression
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  // Tell Webpack which file extensions it adds to the imports finds
+  resolve: {
+    extensions: [".ts", ".js"],
   },
 };
